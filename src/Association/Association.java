e89f4c19502e4ff921aa3b6afd87b5c57725c209
Sujit More <sujitmore21@gmail.com>
2019-07-11 15:32:36 +0530

    Polymorphism: Association

---
+++ src/Association/Association.java
@@ -0,0 +1,26 @@
+package Association;
+
+class CarClass{
+        String carName;
+        int carId;
+        CarClass(String name, int id)
+        {
+            this.carName = name;
+            this.carId = id;
+        }
+    }
+    class Driver extends CarClass{
+        String driverName;
+        Driver(String name, String cname, int cid){
+            super(cname, cid);
+            this.driverName=name;
+        }
+    }
+    class TransportCompany{
+        public void main(String args[])
+        {
+            Driver obj = new Driver("Andy", "Ford", 9988);
+            System.out.println(obj.driverName+" is a driver of car Id: "+obj.carId);
+        }
+    }
+
