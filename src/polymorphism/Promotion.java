26492c37a170eddb7d76124f333bb82e43e12764
Sujit More <sujitmore21@gmail.com>
2019-07-16 17:38:02 +0530

    Polymorphism:promotion

---
+++ src/polymorphism/Promotion.java
@@ -0,0 +1,19 @@
+package polymorphism;
+
+public class Promotion {
+
+    void disp(int a, double b){
+    System.out.println("Method A");
+}
+    void disp(int a, double b, double c){
+        System.out.println("Method B");
+    }
+    public static void main(String args[]){
+        Promotion obj = new Promotion();
+        /* I am passing float value as a second argument but
+         * it got promoted to the type double, because there
+         * wasn't any method having arg list as (int, float)
+         */
+        obj.disp(100, 20.67f);
+    }
+}
